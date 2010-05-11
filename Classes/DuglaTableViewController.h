13d21a24aad4706dc204a371156241b920d8769b
Douglass Turner <douglass.turner@gmail.com>
2010-05-11 18:31:41 -0400

    Simplified the app. Now runnit on iPad.

--- Classes/DuglaTableViewController.h
+++ Classes/DuglaTableViewController.h
@@ -11,22 +11,12 @@
 
 @interface DuglaTableViewController : UITableViewController {
 	
-    NSMutableArray *m_instructors;
-    NSMutableArray *m_teachingAssistants;
-    NSMutableArray *m_guestLecturers;
-	
 	UIPopoverController	*m_popoverController;
 	UILabel				*m_detailDescriptionLabel;
 	
 }
 
-@property(nonatomic,retain)NSMutableArray *instructors;
-@property(nonatomic,retain)NSMutableArray *teachingAssistants;
-@property(nonatomic,retain)NSMutableArray *guestLecturers;
 @property(nonatomic,retain)UIPopoverController *popoverController;
 @property(nonatomic,retain)UILabel *detailDescriptionLabel;
-
-- (NSMutableArray *)namesForSection:(NSInteger)section;
-- (NSString *)nameForIndexPath:(NSIndexPath *)indexPath;
 
 @end
--- Classes/DuglaTableViewController.m
+++ Classes/DuglaTableViewController.m
@@ -8,17 +8,8 @@
 
 #import "DuglaTableViewController.h"
 
-enum {
-    InstructorsGroup = 0,
-    TeachingAssistantsGroup,
-    GuestLecturersGroup
-};
-
 @implementation DuglaTableViewController
 
-@synthesize instructors = m_instructors;
-@synthesize teachingAssistants = m_teachingAssistants;
-@synthesize guestLecturers = m_guestLecturers;
 @synthesize popoverController = m_popoverController;
 @synthesize detailDescriptionLabel = m_detailDescriptionLabel;
 
@@ -27,9 +18,6 @@ enum {
 
 - (void)dealloc {
 	
-    [m_instructors				release], m_instructors = nil;
-    [m_teachingAssistants		release], m_teachingAssistants = nil;
-    [m_guestLecturers			release], m_guestLecturers = nil;
     [m_popoverController		release], m_popoverController = nil;
     [m_detailDescriptionLabel	release], m_detailDescriptionLabel = nil;
     
@@ -43,21 +31,6 @@ enum {
     // Uncomment the following line to preserve selection between presentations.
     self.clearsSelectionOnViewWillAppear = NO;
  
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-	
-	self.instructors = 
-	[[[NSMutableArray alloc] 
-	  initWithObjects:@"Alan", @"Evan", @"Paul M.", @"Alan", @"Evan", @"Paul M.", nil] autorelease];
-	
-	self.teachingAssistants = 
-	[[[NSMutableArray alloc] 
-	  initWithObjects:@"Troy", @"Paul S.", @"Troy", @"Paul S.", nil] autorelease];
-	
-	self.guestLecturers = 
-	[[[NSMutableArray alloc] 
-	  initWithObjects:@"Jason", @"Josh", @"Alex", @"Justin", @"Eric", @"Dan", @"Jason", @"Josh", @"Alex", @"Justin", @"Eric", @"Dan", nil] autorelease];
-		
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -91,15 +64,13 @@ enum {
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
 	
-	return 3;
-//	return 1;
+	return 1;
 	
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 	
-    return [[self namesForSection:section] count];
-//	return 128;
+	return 128;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -110,23 +81,15 @@ enum {
         cell = [[[UITableViewCell alloc] initWithFrame:CGRectZero reuseIdentifier:@"DefaultCell"] autorelease];
     }
     
-    cell.textLabel.text = [self nameForIndexPath:indexPath];
+    cell.textLabel.text = [NSString stringWithFormat:@"%d", indexPath.row];
     
     return cell;
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
 	
-    switch (section) {
-        case InstructorsGroup:
-            return @"Instructors";
-        case TeachingAssistantsGroup:
-            return @"Teaching Assistants";
-        case GuestLecturersGroup:
-            return @"Guest Lecturers";
-        default:
-            return nil;
-    }
+	return @"Numbers";
+	
 }
 
 
@@ -135,27 +98,10 @@ enum {
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
-//    [tableView deselectRowAtIndexPath:indexPath animated:YES];
-	
-	self.detailDescriptionLabel.text = 
-	[NSString stringWithFormat:@"You Selected %@", [self nameForIndexPath:indexPath]];
+	self.detailDescriptionLabel.text = [NSString stringWithFormat:@"You Selected Row %d", indexPath.row];
 	
 	[self.popoverController dismissPopoverAnimated:YES];
 }
-
-// THIS IS THE DEFAULT IMPLEMENTATION PROVIDED BY THE TEMPLATE
-//- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-//	
-//    // Navigation logic may go here. Create and push another view controller.
-//	/*
-//	 <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-//     // ...
-//     // Pass the selected object to the new view controller.
-//	 [self.navigationController pushViewController:detailViewController animated:YES];
-//	 [detailViewController release];
-//	 */
-//}
-
 
 #pragma mark -
 #pragma mark Memory management
@@ -165,28 +111,6 @@ enum {
     [super didReceiveMemoryWarning];
     
     // Relinquish ownership any cached data, images, etc that aren't in use.
-}
-
-#pragma mark -
-#pragma mark My TableView Controller - Helper Methods
-
-- (NSMutableArray *)namesForSection:(NSInteger)section {
-	
-    switch (section) {
-        case InstructorsGroup:
-            return self.instructors;
-        case TeachingAssistantsGroup:
-            return self.teachingAssistants;
-        case GuestLecturersGroup:
-            return self.guestLecturers;
-        default:
-            return nil;
-    }
-}
-
-- (NSString *)nameForIndexPath:(NSIndexPath *)indexPath {
-	
-    return [[self namesForSection:indexPath.section] objectAtIndex:indexPath.row];
 }
 
 @end
